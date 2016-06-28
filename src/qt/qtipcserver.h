428d944bdae248fb9f7b2be941f3393337248910
sexpist <sexpistols@scryptmail.com>
2016-06-28 13:36:04 +0200

    SP

---
+++ src/qt/qtipcserver.h
@@ -0,0 +1,10 @@
+#ifndef QTIPCSERVER_H
+#define QTIPCSERVER_H
+
+// Define SexPistols-Qt message queue name
+#define BITCOINURI_QUEUE_NAME "SexPistolsURI"
+
+void ipcScanRelay(int argc, char *argv[]);
+void ipcInit(int argc, char *argv[]);
+
+#endif // QTIPCSERVER_H
